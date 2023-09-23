78c813b2e194442c6b87f8190bfef21d79115363
ncedile nkonyana <ncedile.nkonyana@sovtech.com>
2023-09-23 21:17:06 +0200

    Adds routes for appliances and appliance and ignore auto generated files from .angular

---
+++ src/app/appliance-management/appliance-management.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-appliance-management',
+  templateUrl: './appliance-management.component.html',
+  styleUrls: ['./appliance-management.component.css']
+})
+export class ApplianceManagementComponent {
+
+}
