52b2167b95bd04c90211a6c4a58c9b0078f1455c
ncedile nkonyana <ncedile.nkonyana@sovtech.com>
2023-09-25 11:32:19 +0200

    Refactor form input controls to use form group builder service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { LoginFormComponent } from './forms/login-form/login-form.component';
-import { CreateAccountFormComponent } from './forms/create-account-form/create-account-form.component';
+import { CreateAccountFormComponent } from './forms/create-account/create-account-form.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { CreateAccountComponent } from './create-account/create-account.component';
 
